d213ab988c8ae59b31493915d69b01b53d55838b
ggutierrez1224 <ggutierrez1224@gmail.com>
2016-09-30 00:02:47 -0400

    Add files via upload

---
+++ Backend.java
@@ -0,0 +1,37 @@
+package model;
+
+
+import javafx.scene.control.Alert;
+
+public class Backend
+{
+    public boolean exists(String name, String artist)
+    {
+        return false;
+    }
+
+    public void add(String name, String artist, String album, String year)
+    {
+
+    }
+
+    public void delete (String name, String Artist)
+    {
+
+    }
+
+    public void edit(Song s, String name, String artist, String album, String year)
+    {
+
+    }
+
+    public void errorPrompt(String header, String content)
+    {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error Dialog");
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+
+        alert.showAndWait();
+    }
+}
