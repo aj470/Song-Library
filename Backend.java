2027cef76a019ee898dcbddff8c6e1abb429cb51
LyleFil <lylefilonuk@ymail.com>
2016-10-01 22:01:17 -0400

    Update Backend.java

--- Backend.java
+++ Backend.java
@@ -3,30 +3,25 @@ package model;
 
 import javafx.scene.control.Alert;
 
-public class Backend
-{
-    public boolean exists(String name, String artist)
-    {
+public class backend {
+	
+    public boolean exists (String name, String artist) {
         return false;
     }
 
-    public void add(String name, String artist, String album, String year)
-    {
+    public void add (String name, String artist, String album, String year) {
 
     }
 
-    public void delete (String name, String Artist)
-    {
+    public void delete (String name, String Artist) {
 
     }
 
-    public void edit(Song s, String name, String artist, String album, String year)
-    {
+    public void edit(Song s, String name, String artist, String album, String year) {
 
     }
 
-    public void errorPrompt(String header, String content)
-    {
+    public void errorPrompt(String header, String content) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error Dialog");
         alert.setHeaderText(header);
